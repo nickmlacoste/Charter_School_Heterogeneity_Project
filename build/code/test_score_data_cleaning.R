b0a9181842c30f2090eb2941b3e59b8e2b98e8c6
nickmlacoste <nickmlacoste@gmail.com>
2024-09-04 10:43:31 -0500

    created initial script and defined paths for cleaning the test score data

---
+++ build/code/test_score_data_cleaning.R
@@ -0,0 +1,35 @@
+# Intro -----------------------------
+
+"
+This script cleans the test score data for charter school heterogeneity
+"
+
+# Imports ------------------
+
+library(randomForest)
+library(tidyverse)
+
+data_path <- "C:/Users/nickm/OneDrive/Acer (new laptop)/Documents/PhD/Tulane University/Projects/Charter School Heterogeneity/data"
+output_path <- "C:/Users/nickm/OneDrive/Acer (new laptop)/Documents/PhD/Tulane University/Projects/Charter School Heterogeneity/Charter_School_Heterogeneity_Project/build/output"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
